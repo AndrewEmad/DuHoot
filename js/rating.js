c110ccf85da9a4fc5e130111122fa4b558146880
Andrew <andrewen2010@yahoo.com>
2018-04-17 21:09:49 +0200

    rating-star

--- js/rating.js
+++ js/rating.js
@@ -1,27 +1,23 @@
-$(".fa-star").hover(function() {
-    $(this).nextAll("i").addClass("fa-star-o");
-    $(this).nextAll("i").removeClass("fa-star");
+$(".rating-star").hover(function() {
+    $(this).nextAll(".fa-star").attr("class","fa fa-star-o rating-star selected");
+    $(this).prevAll(".fa-star-o").attr("class","fa fa-star rating-star");
+    if(!$(this).hasClass("selected")){
+        $(this).attr("class","fa fa-star rating-star");
+    }
+    else{
+        $(this).attr("class","fa fa-star rating-star selected");
+    }
 }, function() {
-    $(this).nextAll(".selected").addClass("fa-star");
-    $(this).nextAll(".selected").removeClass("fa-star-o");
+    $(this).nextAll(".selected").attr("class","fa fa-star rating-star selected");
+    $(this).prevAll(".selected").attr("class","fa fa-star rating-star selected");
+    $(this).prevAll(".fa-star").not(".selected").attr("class","fa fa-star-o rating-star");
+    if(!$(this).hasClass("selected")){
+        $(this).attr("class","fa fa-star-o rating-star");
+    }
 });
 
-
-$(".fa-star-o").hover(function() {
-    $(this).prevAll(".fa-star-o").addClass("fa-star");
-    $(this).prevAll(".fa-star-o").removeClass("fa-star-o");
-    $(this).addClass("fa-star");
-    $(this).removeClass("fa-star-o");
-    alert(1);
-}, function() {
-    $(this).prevAll(".selected").addClass("fa-star");
-    $(this).prevAll(".selected").removeClass("fa-star-o");
-    $(this).addClass("fa-star-o");
-    $(this).removeClass("fa-star");
-});
-
-$(".fa-star, .fa-star-o").on("click", function() {
-    $(this).nextAll("i").addClass("fa-star-o");
-    $(this).addClass("selected");
-    $(this).nextAll("i").removeClass("fa-star selected");
+$(".rating-star").on("click",function(){
+    $(this).prevAll(".rating-star").attr("class","fa fa-star rating-star selected");
+    $(this).attr("class","fa fa-star rating-star selected");
+    $(this).nextAll(".rating-star").attr("class","fa fa-star-o rating-star");
 });
